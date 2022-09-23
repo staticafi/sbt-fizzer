1e623e9d0c6562aa7ce4467ef95a9a10e53bbe12
luxean <lukas.urbn@gmail.com>
2022-09-23 19:56:23 +0200

    feat: add __VERIFIER_ functions

--- code/instrumentation/src/verifier_functions.cpp
+++ code/instrumentation/src/verifier_functions.cpp
@@ -1,8 +1,70 @@
 #include <iomodels/instrumentation_callbacks.hpp>
 
+#include <stdbool.h>
+
 extern "C" {
+int __VERIFIER_nondet_char() {
+    char n;
+    iomodels::on_read_stdin(1, (natural_8_bit*) &n, sizeof(n));
+    return n;
+}
+
+unsigned char __VERIFIER_nondet_uchar() {
+    unsigned char n;
+    iomodels::on_read_stdin(1, (natural_8_bit*) &n, sizeof(n));
+    return n;
+}
+
+_Bool __VERIFIER_nondet_bool() {
+    _Bool n;
+    iomodels::on_read_stdin(1, (natural_8_bit*) &n, sizeof(n));
+    return n;
+}
+
+short __VERIFIER_nondet_short() {
+    short n;
+    iomodels::on_read_stdin(1, (natural_8_bit*) &n, sizeof(n));
+    return n;
+}
+
+unsigned short __VERIFIER_nondet_ushort() {
+    unsigned short n;
+    iomodels::on_read_stdin(1, (natural_8_bit*) &n, sizeof(n));
+    return n;
+}
+
 int __VERIFIER_nondet_int() {
     int n;
+    iomodels::on_read_stdin(1, (natural_8_bit*) &n, sizeof(n));
+    return n;
+}
+
+unsigned int __VERIFIER_nondet_uint() {
+    unsigned int n;
+    iomodels::on_read_stdin(1, (natural_8_bit*) &n, sizeof(n));
+    return n;
+}
+
+long __VERIFIER_nondet_long() {
+    long n;
+    iomodels::on_read_stdin(1, (natural_8_bit*) &n, sizeof(n));
+    return n;
+}
+
+unsigned long __VERIFIER_nondet_ulong() {
+    unsigned long n;
+    iomodels::on_read_stdin(1, (natural_8_bit*) &n, sizeof(n));
+    return n;
+}
+
+float __VERIFIER_nondet_float() {
+    float n;
+    iomodels::on_read_stdin(1, (natural_8_bit*) &n, sizeof(n));
+    return n;
+}
+
+double __VERIFIER_nondet_double() {
+    double n;
     iomodels::on_read_stdin(1, (natural_8_bit*) &n, sizeof(n));
     return n;
 }
