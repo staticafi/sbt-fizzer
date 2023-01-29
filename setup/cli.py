3ff0a06481d270f97f8ad4add5c0d55de5186fd8
Marek Trtik <trtikm@mail.muni.cz>
2023-01-29 16:35:02 +0100

    Added verbose option to cli.py

--- setup/cli.py
+++ setup/cli.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import os
+import sys
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -10,6 +11,7 @@ if __name__ == '__main__':
         ))
     parser.add_argument('-c', '--c_file', help='pathname of a C source to be transformed to the client executable.')
     parser.add_argument('-e', "--erase", action='store_true', help='erase all temporary files in the directory of this script.')
+    parser.add_argument('-v', "--verbose", action='store_true', help='enables the verbose mode.')
     args = parser.parse_args()
 
     script_dir = os.path.abspath(os.path.dirname(__file__))
@@ -19,7 +21,9 @@ if __name__ == '__main__':
     if args.erase:
         for fname in os.listdir(temp_dir):
             ext = os.path.splitext(fname)[1].lower()
-            if ext == ".html" or ext == ".et" or (args.c_file is not None and (ext == ".ll" or fname == "client")):
+            if ext == ".html" or ext == ".et" or (args.c_file is not None and (fname.endswith("_instrumented.ll") or fname == "client")):
+                if args.verbose:
+                    print("> rm " + os.path.join(temp_dir, fname))
                 os.remove(os.path.join(temp_dir, fname))
 
     if args.c_file:
@@ -30,19 +34,27 @@ if __name__ == '__main__':
 
         os.chdir(temp_dir)
 
+        if args.verbose:
+            print("> clang -S -emit-llvm " + c_file)
         os.system("clang -S -emit-llvm " + c_file)
 
+        python_binary = '"' + sys.executable + '"'
+            
         ll_file = os.path.join(temp_dir, os.path.splitext(os.path.basename(c_file))[0] + ".ll")
         if not os.path.isfile(ll_file):
             print("ERROR: Cannot access: " + ll_file)
             exit(1)
-        os.system("python3 " + tools_dir + "/sbt-fizzer_instrument --output_dir ./ " + ll_file)
+        if args.verbose:
+            print("> " + python_binary + " " + tools_dir + "/sbt-fizzer_instrument --output_dir ./ " + ll_file)
+        os.system(python_binary + " " + tools_dir + "/sbt-fizzer_instrument --output_dir ./ " + ll_file)
 
         instrumented_ll_file = os.path.splitext(ll_file)[0] + "_instrumented.ll"
         if not os.path.isfile(instrumented_ll_file):
             print("ERROR: Cannot access: " + instrumented_ll_file)
             exit(1)
-        os.system("python3 " + tools_dir + "/sbt-fizzer_build_client --output_dir ./ " + instrumented_ll_file)
+        if args.verbose:
+            print("> " + python_binary + " " + tools_dir + "/sbt-fizzer_build_client --output_dir ./ " + instrumented_ll_file)
+        os.system(python_binary + " " + tools_dir + "/sbt-fizzer_build_client --output_dir ./ " + instrumented_ll_file)
 
         client_file = os.path.splitext(ll_file)[0] + "_client"
         if not os.path.isfile(instrumented_ll_file):
@@ -52,4 +64,6 @@ if __name__ == '__main__':
         final_file = os.path.join(temp_dir, "client")
         if os.path.isfile(final_file):
             os.remove(final_file)
+        if args.verbose:
+            print("> mv " + client_file + " " + final_file)
         os.rename(client_file, final_file)
