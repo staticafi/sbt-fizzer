2234d4e48cc94a95cec835c2f9c1e91123fcc3f0
Adam Štafa <xstafa@fi.muni.cz>
2023-03-20 15:45:19 +0100

    Add kleeient (klee client)

---
+++ src/tools/kleeient/program_options.cpp
@@ -0,0 +1,39 @@
+#include <client/program_options.hpp>
+#include <client/program_info.hpp>
+#include <fuzzing/fuzzers_map.hpp>
+#include <utility/assumptions.hpp>
+#include <stdexcept>
+#include <iostream>
+
+program_options::program_options(int argc, char* argv[])
+    : program_options_default(argc, argv)
+{
+    add_option("input", "Run the instrumented file with the specified hexadecimal input and output the trace.", "1");
+
+    add_option("address", "The address of the SBT-Fizzer server in IPv4 dotted decimal form or IPv6 hexadecimal notation.", "1");
+    add_value("address", "127.0.0.1");
+
+    add_option("port", "The port of the SBT-Fizzer server.", "1");
+    add_value("port", "42086");
+}
+
+static program_options_ptr  global_program_options;
+
+void initialise_program_options(int argc, char* argv[])
+{
+    ASSUMPTION(!global_program_options.operator bool());
+    global_program_options = program_options_ptr(new program_options(argc,argv));
+}
+
+program_options_ptr get_program_options()
+{
+    ASSUMPTION(global_program_options.operator bool());
+    return global_program_options;
+}
+
+std::ostream& operator<<(std::ostream& ostr, program_options_ptr const& options)
+{
+    ASSUMPTION(options.operator bool());
+    options->operator<<(ostr);
+    return ostr;
+}
---
+++ src/tools/kleeient/program_options.hpp
@@ -0,0 +1,20 @@
+#ifndef TOOL_CLIENT_PROGRAM_OPTIONS_HPP_INCLUDED
+#   define TOOL_CLIENT_PROGRAM_OPTIONS_HPP_INCLUDED
+
+#   include <utility/program_options_base.hpp>
+#   include <memory>
+
+class program_options : public program_options_default
+{
+public:
+    program_options(int argc, char* argv[]);
+};
+
+typedef std::shared_ptr<program_options const> program_options_ptr;
+
+void initialise_program_options(int argc, char* argv[]);
+program_options_ptr get_program_options();
+
+std::ostream& operator<<(std::ostream& ostr, program_options_ptr const& options);
+
+#endif
