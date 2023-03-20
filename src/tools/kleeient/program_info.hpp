007083d04ab6b95f36c1d070615a1f0032bdb83b
Adam Štafa <xstafa@fi.muni.cz>
2023-03-20 15:45:19 +0100

    Add kleeient (klee client)

---
+++ src/tools/kleeient/program_info.hpp
@@ -0,0 +1,10 @@
+#ifndef TOOL_KLEEIENT_PROGRAM_INFO_HPP_INCLUDED
+#   define TOOL_KLEEIENT_PROGRAM_INFO_HPP_INCLUDED
+
+#   include <string>
+
+std::string  get_program_name();
+std::string  get_program_version();
+std::string  get_program_description();
+
+#endif
