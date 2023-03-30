7bca079530e51fa2cd3e317a3e86a54df76893f1
Marek Trtik <trtikm@gmail.com>
2023-03-30 23:37:23 +0200

    Fixed assumtion failure in  transmition of stdin bits.

--- src/iomodels/src/stdin_replay_bits_then_repeat_85.cpp
+++ src/iomodels/src/stdin_replay_bits_then_repeat_85.cpp
@@ -29,6 +29,7 @@ void  stdin_replay_bits_then_repeat_85::save(connection::message&  ostr) const
 
     vecu8  bytes;
     bits_to_bytes(bits, bytes);
+    ostr << (natural_16_bit)bits.size();
     ostr << (natural_16_bit)bytes.size();
     for (natural_8_bit  byte : bytes)
         ostr << byte;
@@ -41,12 +42,14 @@ void  stdin_replay_bits_then_repeat_85::save(connection::message&  ostr) const
 
 void  stdin_replay_bits_then_repeat_85::load(connection::message&  istr)
 {
-    natural_16_bit  num_bytes;
+    natural_16_bit  num_bits, num_bytes;
+    istr >> num_bits;
     istr >> num_bytes;
     vecu8  bytes(num_bytes, 0);
     for (natural_16_bit  i = 0U; i < num_bytes; ++i)
         istr >> bytes.at(i);
     bytes_to_bits(bytes, bits);
+    bits.resize(num_bits);
 
     ASSUMPTION(bits.size() <= max_bits());
 
@@ -76,38 +79,6 @@ void  stdin_replay_bits_then_repeat_85::read(location_id const  id, natural_8_bi
         ptr[j] = value;
     }
     counts.push_back(8U * count);
-
-    /*
-    // WARNING: If the server sends input s.t. "bits.size() % 8 != 0", then this
-    //          implementation will skip the last "bits.size() - 8 * (bits.size() / 8)" bits.
-
-    natural_8_bit to_replay = (natural_8_bit)std::min((bits.size() - cursor) / 8, (std::size_t)count);
-    for (natural_8_bit  j = 0U; j != to_replay; ++j)
-    {
-        ptr[j] = 0;
-        for (natural_8_bit  i = 0; i != 8; ++i)
-        {
-            ptr[j] |= (natural_8_bit)((bits.at(cursor) ? 1U : 0U) << (7U - i));
-            ++cursor;
-        }
-    }
-
-    natural_8_bit leftover = count - to_replay;
-    memset((void*) (ptr + to_replay), 85, leftover);
-    if (bits.size() + 8 * leftover <= get_max_bits())
-    {
-        for (natural_8_bit  j = 0; j != leftover; ++j)
-        {
-            for (natural_8_bit  i = 0; i != 8; ++i)
-            {
-                bits.push_back(ptr[j] & (1 << (7U - i)));
-                ++cursor;
-            }
-        }
-        
-        counts.push_back(8U * count);
-    }
-    */
 }
 
 
