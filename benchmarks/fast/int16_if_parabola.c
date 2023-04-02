906a4cd4e890ee196c5b148b66e00576030c5eb0
Adam Štafa <adam.stafa11@gmail.com>
2023-04-03 00:08:05 +0200

    Fix wrong __VERIFIER_nondet call in benchmark

--- benchmarks/fast/int16_if_parabola.c
+++ benchmarks/fast/int16_if_parabola.c
@@ -3,7 +3,7 @@ extern short __VERIFIER_nondet_short();
 int main()
 {
     short x;
-    x = __VERIFIER_nondet_short(x);
+    x = __VERIFIER_nondet_short();
     if (-x*x+(short)6*x-(short)8 == (short)0)  // Roots are: 2 and 4.
         return 1;
     return 0;
