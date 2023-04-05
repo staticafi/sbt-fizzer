a9450e1417b8d2948067df0d0ecef5b0b90d8575
Adam Štafa <adam.stafa11@gmail.com>
2023-04-05 19:17:06 +0200

    Fix wrong __VERIFIER_nondet calls in benchmarks

--- benchmarks/fast/float_if_x_eq_c.c
+++ benchmarks/fast/float_if_x_eq_c.c
@@ -3,7 +3,7 @@ extern float __VERIFIER_nondet_float();
 int main()
 {
     float x;
-    x = __VERIFIER_nondet_float(x);
+    x = __VERIFIER_nondet_float();
     if (x == -123.4567f) // -123.4567f = 0xC2F6E9D5 hex = 1100 0010 1111 0110 1110 1001 1101 0101 bin
         return 1;
     return 0;
--- benchmarks/fast/int32_logical_or_two_vars.c
+++ benchmarks/fast/int32_logical_or_two_vars.c
@@ -3,8 +3,8 @@ extern int __VERIFIER_nondet_int();
 int main()
 {
     int x, y;
-    x = __VERIFIER_nondet_int(x);
-    y = __VERIFIER_nondet_int(y);
+    x = __VERIFIER_nondet_int();
+    y = __VERIFIER_nondet_int();
 
     // represents: if (x < 0 || x > 3 || y < 0 || y > 23) return 0; else return 1;
     if (x < 0)
