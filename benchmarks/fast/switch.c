a2f74d54f1398707a74cb78f4b761fda496fb7e6
Marek Trtik <trtikm@gmail.com>
2023-05-21 16:02:01 +0200

    Added "switch" benchmark

---
+++ benchmarks/fast/switch.c
@@ -0,0 +1,14 @@
+int __VERIFIER_nondet_int();
+
+int main()
+{
+    switch (__VERIFIER_nondet_int())
+    {
+        case 0: return 1;
+        case 1: return 10;
+        case 2: return 100;
+        case 3: return 1000;
+        default: break;
+    }
+    return 0;
+}
