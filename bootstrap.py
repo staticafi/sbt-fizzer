1e5018f3d3a329ef2d2311bb9a4f02bd0bec07bb
Marek Trtik <trtikm@gmail.com>
2024-03-02 13:33:34 +0100

    Added scripts aiding work with git submodules.

---
+++ bootstrap.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import subprocess
+
+subprocess.run(["git", "init"])
+subprocess.run(["git", "-c", "protocol.file.allow=always", "submodule", "add", "https://github.com/staticafi/libutility.git", "./src/utility"])
+subprocess.run(["git", "-c", "protocol.file.allow=always", "submodule", "add", "https://github.com/staticafi/libsala.git", "./src/sala"])
+subprocess.run(["git", "-c", "protocol.file.allow=always", "submodule", "add", "https://github.com/staticafi/libllvmutl.git", "./src/llvmutl"])
+subprocess.run(["git", "-c", "protocol.file.allow=always", "submodule", "add", "https://github.com/staticafi/binsalac.git", "./src/tools/salac"])
+subprocess.run(["git", "-c", "protocol.file.allow=always", "submodule", "add", "https://github.com/staticafi/binsalat.git", "./src/tools/salat"])
+subprocess.run(["git", "-c", "protocol.file.allow=always", "submodule", "add", "https://github.com/staticafi/datsalat.git", "./benchmarks/salat"])
