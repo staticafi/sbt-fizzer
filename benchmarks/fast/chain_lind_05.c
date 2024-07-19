20163a36e994af0bd6b23bf69190ac62e62e82aa
Marek Trtik <trtikm@gmail.com>
2024-07-19 23:58:08 +0200

    Added more benchmarks for chain mini

---
+++ benchmarks/fast/chain_lind_05.c
@@ -0,0 +1,15 @@
+extern double __VERIFIER_nondet_double(void);
+
+int main() {
+    double x0 = __VERIFIER_nondet_double();
+    double x1 = __VERIFIER_nondet_double();
+
+    // a solution:  x0 = 2, x1 = -1;
+
+    if ( 2.00*x0 - x1 - 3.0 >= 0.0)
+    if (           x1 + 2.0 >  0.0)
+    if ( 1.25*x0 + x1 - 2.5 <= 0.0)
+        return 1;
+
+    return 0;
+}
