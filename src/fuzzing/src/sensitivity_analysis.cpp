4e983a0cbe386000125f2d58bf42eca6a3204a09
Marek Trtik <trtikm@gmail.com>
2024-09-02 00:54:33 +0200

    Separating special_values_analysis from the sensitivity analysis

--- src/fuzzing/src/sensitivity_analysis.cpp
+++ src/fuzzing/src/sensitivity_analysis.cpp
@@ -12,8 +12,6 @@ sensitivity_analysis::sensitivity_analysis()
     , bits_and_types{ nullptr }
     , trace{ nullptr }
     , mutated_bit_index{ 0 }
-    , mutated_type_index{ 0 }
-    , mutated_value_index{ 0 }
     , probed_bit_start_index{ 0 }
     , probed_bit_end_index{ 0 }
     , node{ nullptr }
@@ -25,16 +23,15 @@ sensitivity_analysis::sensitivity_analysis()
 {}
 
 
-bool  sensitivity_analysis::is_mutated_bit_index_valid() const
+bool  sensitivity_analysis::is_disabled() const
 {
-    return mutated_bit_index < node->get_num_stdin_bits();
+    return false;
 }
 
 
-bool  sensitivity_analysis::is_mutated_type_index_valid() const
+bool  sensitivity_analysis::is_mutated_bit_index_valid() const
 {
-    return mutated_type_index < node->best_stdin->types.size() &&
-                node->best_stdin->type_end_bit_index(mutated_type_index) < node->get_num_stdin_bits();
+    return mutated_bit_index < node->get_num_stdin_bits();
 }
 
 
@@ -61,8 +58,6 @@ void  sensitivity_analysis::start(branching_node* const  node_ptr, natural_32_bi
     bits_and_types = node_ptr->best_stdin;
     trace = node_ptr->best_trace;
     mutated_bit_index = 0;
-    mutated_type_index = 0;
-    mutated_value_index = 0;
     node = node_ptr;
     execution_id = execution_id_;
     changed_nodes.clear();
@@ -82,7 +77,7 @@ void  sensitivity_analysis::stop()
     if (!is_busy())
         return;
 
-    if (is_mutated_bit_index_valid() || is_mutated_type_index_valid())
+    if (is_mutated_bit_index_valid())
     {
         stopped_early = true;
 
@@ -116,194 +111,22 @@ bool  sensitivity_analysis::generate_next_input(vecb&  bits_ref)
 
     if (is_mutated_bit_index_valid())
     {
-        bits_ref = bits_and_types->bits;
-        bits_ref.at(mutated_bit_index) = !bits_ref.at(mutated_bit_index);
-
-        probed_bit_start_index = 8 * (mutated_bit_index / 8);
-        probed_bit_end_index = probed_bit_start_index + 8;
-
-        ++mutated_bit_index;
-    }
-    else if (!generate_next_typed_value(bits_ref))
-    {
-        for (branching_node* n = node; n != nullptr; n = n->predecessor)
-        {
-            if (!n->sensitivity_performed)
-                changed_nodes.insert(n);
-            n->sensitivity_performed = true;
-            n->sensitivity_start_execution = execution_id;
-        }
-
         stop();
         return false;
     }
+
+    bits_ref = bits_and_types->bits;
+    bits_ref.at(mutated_bit_index) = !bits_ref.at(mutated_bit_index);
+
+    probed_bit_start_index = 8 * (mutated_bit_index / 8);
+    probed_bit_end_index = probed_bit_start_index + 8;
+
+    ++mutated_bit_index;
 
     ++statistics.generated_inputs;
 
     return true;
 }
-
-
-template<typename T, int N>
-bool  sensitivity_analysis::write_bits(vecb&  bits_ref, T const  (&values)[N])
-{
-    if (mutated_value_index >= N)
-    {
-        mutated_value_index = 0U;
-        return false;
-    }
-
-    probed_bit_start_index = node->best_stdin->type_start_bit_index(mutated_type_index);
-    probed_bit_end_index = probed_bit_start_index + 8 * sizeof(T);
-
-    vecb  bits;
-    natural_8_bit const* const  value_ptr = (natural_8_bit const*)&values[mutated_value_index];
-    bytes_to_bits(value_ptr, value_ptr + sizeof(T), bits);
-
-    bits_ref = bits_and_types->bits;
-    std::copy(bits.begin(), bits.end(), std::next(bits_ref.begin(), probed_bit_start_index));
-
-    ++mutated_value_index;
-    return true;
-}
-
-
-bool  sensitivity_analysis::generate_next_typed_value(vecb&  bits_ref)
-{
-    for ( ; is_mutated_type_index_valid(); ++mutated_type_index)
-        switch (node->best_stdin->types.at(mutated_type_index))
-        {
-        case type_of_input_bits::BOOLEAN:
-            break;
-
-        case type_of_input_bits::SINT8:
-            {
-                static integer_8_bit const  values[] = {
-                        std::numeric_limits<integer_8_bit>::min(),
-                        std::numeric_limits<integer_8_bit>::max(),
-                        };
-                if (write_bits(bits_ref, values))
-                    return true;
-            }
-            break;
-        case type_of_input_bits::UINT8:
-        case type_of_input_bits::UNTYPED8:
-            {
-                static natural_8_bit const  values[] = {
-                        std::numeric_limits<natural_8_bit>::max(),
-                        };
-                if (write_bits(bits_ref, values))
-                    return true;
-            }
-            break;
-
-        case type_of_input_bits::SINT16:
-            {
-                static integer_16_bit const  values[] = {
-                        std::numeric_limits<integer_16_bit>::min(),
-                        std::numeric_limits<integer_16_bit>::max(),
-                        };
-                if (write_bits(bits_ref, values))
-                    return true;
-            }
-            break;
-        case type_of_input_bits::UINT16:
-        case type_of_input_bits::UNTYPED16:
-            {
-                static natural_16_bit const  values[] = {
-                        std::numeric_limits<natural_16_bit>::max(),
-                        };
-                if (write_bits(bits_ref, values))
-                    return true;
-            }
-            break;
-
-        case type_of_input_bits::SINT32:
-            {
-                static integer_32_bit const  values[] = {
-                        std::numeric_limits<integer_32_bit>::min(),
-                        std::numeric_limits<integer_32_bit>::max(),
-                        };
-                if (write_bits(bits_ref, values))
-                    return true;
-            }
-            break;
-        case type_of_input_bits::UINT32:
-        case type_of_input_bits::UNTYPED32:
-            {
-                static natural_32_bit const  values[] = {
-                        std::numeric_limits<natural_32_bit>::max(),
-                        };
-                if (write_bits(bits_ref, values))
-                    return true;
-            }
-            break;
-
-        case type_of_input_bits::SINT64:
-            {
-                static integer_64_bit const  values[] = {
-                        std::numeric_limits<integer_64_bit>::min(),
-                        std::numeric_limits<integer_64_bit>::max(),
-                        };
-                if (write_bits(bits_ref, values))
-                    return true;
-            }
-            break;
-        case type_of_input_bits::UINT64:
-        case type_of_input_bits::UNTYPED64:
-            {
-                static natural_64_bit const  values[] = {
-                        std::numeric_limits<natural_64_bit>::max(),
-                        };
-                if (write_bits(bits_ref, values))
-                    return true;
-            }
-            break;
-
-        case type_of_input_bits::FLOAT32:
-            {
-                static float_32_bit const  values[] = {
-                        -std::numeric_limits<float_32_bit>::infinity(),
-                        std::numeric_limits<float_32_bit>::lowest(),
-                        -std::numeric_limits<float_32_bit>::min(),
-                        -std::numeric_limits<float_32_bit>::epsilon(),
-                        std::numeric_limits<float_32_bit>::epsilon(),
-                        std::numeric_limits<float_32_bit>::min(),
-                        std::numeric_limits<float_32_bit>::max(),
-                        std::numeric_limits<float_32_bit>::infinity(),
-                        std::numeric_limits<float_32_bit>::quiet_NaN(),
-                        std::numeric_limits<float_32_bit>::signaling_NaN(),
-                        };
-                if (write_bits(bits_ref, values))
-                    return true;
-            }
-            break;
-        case type_of_input_bits::FLOAT64:
-            {
-                static float_64_bit const  values[] = {
-                        -std::numeric_limits<float_64_bit>::infinity(),
-                        std::numeric_limits<float_64_bit>::lowest(),
-                        -std::numeric_limits<float_64_bit>::min(),
-                        -std::numeric_limits<float_64_bit>::epsilon(),
-                        std::numeric_limits<float_64_bit>::epsilon(),
-                        std::numeric_limits<float_64_bit>::min(),
-                        std::numeric_limits<float_64_bit>::max(),
-                        std::numeric_limits<float_64_bit>::infinity(),
-                        std::numeric_limits<float_64_bit>::quiet_NaN(),
-                        std::numeric_limits<float_64_bit>::signaling_NaN(),
-                        };
-                if (write_bits(bits_ref, values))
-                    return true;
-            }
-            break;
-
-        default:
-            UNREACHABLE();
-            break;
-        }
-    return false;
-}
-
 
 
 void  sensitivity_analysis::process_execution_results(execution_trace_pointer const  trace_ptr, branching_node* const  entry_branching_ptr)
