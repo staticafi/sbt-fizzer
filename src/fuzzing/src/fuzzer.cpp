6bd3bbdbd9d292573e68393da59ebdcb4ef89754
Marek Trtik <trtikm@gmail.com>
2024-11-13 19:12:31 +0100

    Fuzzer: use bitflip rather than finish

--- src/fuzzing/src/fuzzer.cpp
+++ src/fuzzing/src/fuzzer.cpp
@@ -1773,7 +1773,7 @@ void  fuzzer::select_next_state()
 
     if (winner == nullptr)
     {
-        if (sala_program_ptr == nullptr || (!leaf_branchings.empty() && input_flow_thread.is_busy() || input_flow_thread.is_finished()))
+        if (sala_program_ptr == nullptr || (!leaf_branchings.empty() && !uncovered_branchings.empty()))
         {
             if (bitflip.is_ready())
                 bitflip.start(leaf_branchings);
