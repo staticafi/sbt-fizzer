59bc263be4c94490caba7c0dcf8a49bddd502036
Jakub Máca <xmaca@fi.muni.cz>
2024-12-22 15:20:34 +0100

    feat: loop processing

--- src/fuzzing/include/fuzzing/iid_vector_analysis.hpp
+++ src/fuzzing/include/fuzzing/iid_vector_analysis.hpp
@@ -1,17 +1,15 @@
 #pragma once
 
+#include <iostream>
 #include <map>
 #include <set>
 #include <unordered_map>
+#include <vector>
 
 #include <fuzzing/branching_node.hpp>
 #include <fuzzing/sensitivity_analysis.hpp>
+#include <instrumentation/instrumentation_types.hpp>
 
-struct node_direction;
-using loop_ending_to_bodies = std::map< std::pair< location_id, bool >, std::set< node_direction > >;
-using loop_endings = std::map< location_id, bool >;
-using loop_head_to_bodies_t = std::unordered_map< location_id, std::unordered_set< location_id > >;
-using loop_head_to_loaded_bits_t = std::unordered_map< location_id, std::tuple< natural_32_bit, natural_32_bit > >;
 
 namespace fuzzing
 {
@@ -20,8 +18,19 @@ struct direction_vector {};
 struct node_direction {
     location_id node_id;
     bool branching_direction;
+
+    auto operator<=>( node_direction const& other ) const;
+    bool operator==( node_direction const& other ) const;
+    friend std::ostream& operator<<( std::ostream& os, const node_direction& nav )
+    {
+        return os << nav.node_id.id << " " << ( nav.branching_direction ? "right" : "left" );
+    }
 };
 
+using loop_ending_to_bodies = std::map< std::pair< location_id, bool >, std::set< node_direction > >;
+using loop_endings = std::map< location_id, bool >;
+using loop_head_to_bodies_t = std::unordered_map< location_id, std::unordered_set< location_id > >;
+using loop_head_to_loaded_bits_t = std::unordered_map< location_id, std::tuple< natural_32_bit, natural_32_bit > >;
 
 struct equation {
     std::vector< float > values;
@@ -32,9 +41,11 @@ struct equation {
 struct equation_matrix {
     equation_matrix get_submatrix( std::set< node_direction > const& subset, bool unique ) const;
     void add_equation( branching_node* end_node );
+    bool contains( node_direction const& node ) const;
 
 private:
     std::vector< equation > matrix;
+    std::set< node_direction > nodes;
 };
 
 
@@ -42,10 +53,11 @@ struct iid_node_dependence_props {
     std::unordered_map< location_id::id_type, float > generate_probabilities();
     void process_node( branching_node* end_node );
 
+    void print_dependencies();
+
 private:
     loop_endings get_loop_heads_ending( branching_node* end_node,
-                                        const loop_head_to_bodies_t& loop_heads_to_bodies,
-                                        const std::vector< fuzzer::loop_boundary_props >& loops ) const;
+                                        loop_head_to_bodies_t& loop_heads_to_bodies );
     void compute_dependencies_by_loading( branching_node* end_node,
                                           const loop_head_to_bodies_t& loop_heads_to_bodies,
                                           const loop_endings& loop_heads_ending );
@@ -60,9 +72,13 @@ private:
 struct iid_dependencies {
     void update_non_iid_nodes( sensitivity_analysis& sensitivity );
     void process_node_dependence( branching_node* node );
+    iid_node_dependence_props& get_props( location_id id );
+    std::vector< location_id > get_iid_nodes();
 
 private:
     std::unordered_map< location_id, iid_node_dependence_props > id_to_equation_map;
     std::set< location_id > non_iid_nodes;
 };
+
+std::vector< node_direction > get_path( branching_node* node );
 } // namespace fuzzing
--- src/fuzzing/src/iid_vector_analysis.cpp
+++ src/fuzzing/src/iid_vector_analysis.cpp
@@ -1,26 +1,86 @@
+#include <fuzzing/branching_node.hpp>
 #include <fuzzing/fuzzer.hpp>
 #include <fuzzing/iid_vector_analysis.hpp>
+#include <instrumentation/instrumentation_types.hpp>
+#include <iostream>
+#include <string>
 #include <utility/timeprof.hpp>
+
+// ------------------------------------------------------------------------------------------------
+auto fuzzing::node_direction::operator<=>( node_direction const& other ) const
+{
+    if ( auto const cmp = node_id.id <=> other.node_id.id; cmp != 0 )
+        return cmp;
+
+    return branching_direction <=> other.branching_direction;
+}
+
+// ------------------------------------------------------------------------------------------------
+bool fuzzing::node_direction::operator==( node_direction const& other ) const
+{
+    return node_id.id == other.node_id.id && branching_direction == other.branching_direction;
+}
+
+// ------------------------------------------------------------------------------------------------
+void fuzzing::equation_matrix::add_equation( branching_node* end_node )
+{
+    std::vector< node_direction > path = get_path( end_node );
+    for ( const node_direction& nav : path ) {
+        nodes.insert( nav );
+    }
+}
+
+// ------------------------------------------------------------------------------------------------
+bool fuzzing::equation_matrix::contains( node_direction const& node ) const { return nodes.contains( node ); }
+
+
+// ------------------------------------------------------------------------------------------------
+std::unordered_map< location_id::id_type, float > fuzzing::iid_node_dependence_props::generate_probabilities()
+{
+    print_dependencies();
+
+    return std::unordered_map< location_id::id_type, float >();
+}
 
 // ------------------------------------------------------------------------------------------------
 void fuzzing::iid_node_dependence_props::process_node( branching_node* end_node )
 {
     loop_head_to_bodies_t loop_heads_to_bodies;
-    std::vector< fuzzer::loop_boundary_props > loops;
-    fuzzing::fuzzer::detect_loops_along_path_to_node( end_node, loop_heads_to_bodies, &loops );
-
-    loop_endings loop_heads_ending = get_loop_heads_ending( end_node, loop_heads_to_bodies, loops );
+    loop_endings loop_heads_ending = get_loop_heads_ending( end_node, loop_heads_to_bodies );
 
     compute_dependencies_by_loading( end_node, loop_heads_to_bodies, loop_heads_ending );
     compute_dependencies_by_loops( loop_heads_to_bodies, loop_heads_ending );
+
+    matrix.add_equation( end_node );
 }
 
 // ------------------------------------------------------------------------------------------------
-std::map< location_id, bool > fuzzing::iid_node_dependence_props::get_loop_heads_ending(
-    branching_node* end_node,
-    const loop_head_to_bodies_t& loop_heads_to_bodies,
-    const std::vector< fuzzer::loop_boundary_props >& loops ) const
+void fuzzing::iid_node_dependence_props::print_dependencies()
 {
+    std::cout << "# Dependencies:" << std::endl;
+    std::cout << "## Dependencies by loops:" << std::endl;
+    for ( const auto& [ loop, nodes ] : dependencies_by_loops ) {
+        for ( const auto& body : nodes ) {
+            std::cout << "- " << "`(" << body << ") → " << loop.first.id << "`" << std::endl;
+        }
+    }
+
+    std::cout << "## Dependencies by loading:" << std::endl;
+    for ( const auto& [ loading, nodes ] : dependencies_by_loading ) {
+        for ( const auto& body : nodes ) {
+            std::cout << "- " << "`(" << body << ") → " << loading.first.id << "`" << std::endl;
+        }
+    }
+}
+
+// ------------------------------------------------------------------------------------------------
+std::map< location_id, bool >
+fuzzing::iid_node_dependence_props::get_loop_heads_ending( branching_node* end_node,
+                                                           loop_head_to_bodies_t& loop_heads_to_bodies )
+{
+    std::vector< fuzzer::loop_boundary_props > loops;
+    fuzzing::fuzzer::detect_loops_along_path_to_node( end_node, loop_heads_to_bodies, &loops );
+
     std::map< location_id, bool > loop_heads_ending;
 
     auto is_outside_loop = [ & ]( branching_node* successor,
@@ -79,18 +139,28 @@ void fuzzing::iid_node_dependence_props::compute_dependencies_by_loading( branch
     while ( node != nullptr ) {
         auto node_id = node->get_location_id();
 
-        for ( const auto& bit_index : node->sensitive_stdin_bits ) {
-            for ( const auto& [ loop_head, values ] : loading_loops ) {
-                if ( !loop_heads_ending.contains( loop_head ) ) {
-                    continue;
-                }
+        for ( const auto& [ loop_head, values ] : loading_loops ) {
+            if ( !loop_heads_ending.contains( loop_head ) ) {
+                continue;
+            }
 
-                bool loop_head_direction = loop_heads_ending.at( loop_head );
+            bool loop_head_direction = loop_heads_ending.at( loop_head );
 
-                auto& [ min, max ] = values;
-                if ( bit_index >= min && bit_index <= max ) {
-                    dependencies_by_loading[ { loop_head, loop_head_direction } ].emplace( node_id, true );
-                    dependencies_by_loading[ { loop_head, loop_head_direction } ].emplace( node_id, false );
+            const auto& [ min, max ] = values;
+
+            auto it = std::find_if( node->sensitive_stdin_bits.begin(),
+                                    node->sensitive_stdin_bits.end(),
+                                    [ & ]( natural_32_bit bit_index ) {
+                                        return bit_index >= min && bit_index <= max;
+                                    } );
+
+            if ( it != node->sensitive_stdin_bits.end() ) {
+                for ( bool direction : { true, false } ) {
+                    node_direction node_id_direction = { node_id, direction };
+
+                    if ( matrix.contains( node_id_direction ) ) {
+                        dependencies_by_loading[ { loop_head, loop_head_direction } ].insert( node_id_direction );
+                    }
                 }
             }
         }
@@ -111,14 +181,18 @@ void fuzzing::iid_node_dependence_props::compute_dependencies_by_loops( const lo
         bool loop_head_end_direction = loop_heads_ending.at( loop_head );
 
         for ( const auto& body : loop_bodies ) {
-            dependencies_by_loops[ { loop_head, loop_head_end_direction } ].emplace( body, true );
-            dependencies_by_loops[ { loop_head, loop_head_end_direction } ].emplace( body, false );
+            for ( bool direction : { true, false } ) {
+                node_direction node_id_direction = { body, direction };
+
+                if ( matrix.contains( node_id_direction ) )
+                    dependencies_by_loops[ { loop_head, loop_head_end_direction } ].insert( node_id_direction );
+            }
         }
     }
 }
 
 // ------------------------------------------------------------------------------------------------
-void fuzzing::iid_dependencies::update_non_iid_nodes( sensitivity_analysis& sensitivity ) 
+void fuzzing::iid_dependencies::update_non_iid_nodes( sensitivity_analysis& sensitivity )
 {
     for ( branching_node* node : sensitivity.get_changed_nodes() ) {
         if ( node->is_did_branching() ) {
@@ -140,4 +214,39 @@ void fuzzing::iid_dependencies::process_node_dependence( branching_node* node )
 
     iid_node_dependence_props& props = id_to_equation_map[ node->get_location_id() ];
     props.process_node( node );
+}
+
+// ------------------------------------------------------------------------------------------------
+fuzzing::iid_node_dependence_props& fuzzing::iid_dependencies::get_props( location_id id )
+{
+    return id_to_equation_map.at( id );
+}
+
+// ------------------------------------------------------------------------------------------------
+std::vector< location_id > fuzzing::iid_dependencies::get_iid_nodes()
+{
+    std::vector< location_id > result;
+    for ( const auto& [ key, _ ] : id_to_equation_map ) {
+        result.push_back( key );
+    }
+
+    return result;
+}
+
+// ------------------------------------------------------------------------------------------------
+std::vector< fuzzing::node_direction > fuzzing::get_path( branching_node* node )
+{
+    std::vector< node_direction > result;
+
+    branching_node* current = node;
+    while ( current != nullptr ) {
+        branching_node* predecessor = current->predecessor;
+        if ( predecessor != nullptr ) {
+            node_direction nav = { predecessor->get_location_id(), predecessor->successor_direction( current ) };
+            result.push_back( nav );
+        }
+        current = predecessor;
+    }
+
+    return result;
 }
