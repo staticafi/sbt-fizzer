ce547c08eb9c2316961e25f9cd8fd49f06d2dca0
Jakub Máca <xmaca@fi.muni.cz>
2025-01-08 19:39:17 +0100

    feat: add new benchmark for input cycle and update execution counts

---
+++ benchmarks/iid_testing/input_cycle_dec_b.c
@@ -0,0 +1,48 @@
+#include <stdbool.h>
+
+extern char __VERIFIER_nondet_char();
+extern short __VERIFIER_nondet_short();
+
+
+int main()
+{
+    char s[ 50 ];
+    {
+        short n;
+        n = __VERIFIER_nondet_short();
+        if ( n <= 0 ) // ID: 1
+            return -1;
+        if ( n >= sizeof( s ) / sizeof( s[ 0 ] ) ) // ID: 2
+            return -1;
+        for ( short i = 0; i < n; ++i ) // ID: 3
+            s[ i ] = __VERIFIER_nondet_char();
+        if ( s[ n - 1 ] != '\0' ) // ID: 4
+            return -1;
+    }
+    {
+        short loop_count;
+        loop_count = __VERIFIER_nondet_short();
+        if ( loop_count > 50 ) // ID: 5
+            return -1;
+
+        int i = 0, k = 0;
+
+        for ( short index = 0; index < loop_count; ++index ) { // ID: 6
+            i = 0;
+            while ( true ) {
+                if ( s[ i ] == '\0' ) // ID: 7
+                    break;
+                if ( s[ i ] == 'A' ) // ID: 8
+                    ++k;
+                if ( s[ i ] == 'B' ) // ID: 9
+                    --k;
+                ++i;
+            }
+        }
+
+        if ( k == 10 ) // ID: 10
+            return 1;
+
+        return 0;
+    }
+}
